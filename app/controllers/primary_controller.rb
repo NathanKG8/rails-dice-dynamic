f21f5b4930c3f924fe6bc57e729d60280e083b44
NathanKG8 <164825742+NathanKG8@users.noreply.github.com>
2024-04-22 23:52:46 +0000

    Done?

---
+++ app/controllers/primary_controller.rb
@@ -0,0 +1,19 @@
+class PrimaryController < ApplicationController
+  def homepage
+    render({ :template => "die_templates/homepage"})
+  end
+
+  def diceroll
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    
+    @rolls = []
+  
+    @num_dice.times do
+      die = rand(1..@sides)
+  
+      @rolls.push(die)
+    end
+    render({ :template => "die_templates/roll"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-
+  get("/", {:controller => "primary", :action => "homepage"}) 
+  
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "primary", :action => "diceroll"}) 
 end
